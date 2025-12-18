e41d68b54417723e12c50a7386cf71f2161fa985
Leah J Hudson <137237352+leahjordanhudson@users.noreply.github.com>
2025-12-18 14:22:58 +1100

    Update Run_PEB_Reversal.m
    Update to table location in the manuscript

--- analysis/Run_PEB_Reversal.m
+++ analysis/Run_PEB_Reversal.m
@@ -1,6 +1,6 @@
 %% Parametric Empirical Bayes (PEB), reversal learning model
 % This script reproduces the effective connectivity results
-% reported in the Hudson et al. manuscript (Figure 3a, Table 1).
+% reported in the Hudson et al. manuscript (Figure 3a, Supplementary Table 3).
 
 % -----------------------------------------------------------------------
 % Please ensure that your SPM12 folder (r7771) is listed in your MATLAB set
